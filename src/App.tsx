c2791fc936a06b5e4fd9e53d932cc7e1b65c4219
laurentvuillaume <figura.graphik@gmail.com>
2025-10-24 01:10:13 +0200

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
-import { useState, useEffect } from "react";
-import type { Product } from "../types";
 import ProductList from "./components/ProductList";
+import Header from "./components/Header";
 
 const App = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        if (!response.ok) {
-          throw new Error("Failed to fetch products");
-        }
-        const data: Product[] = await response.json();
-        setProducts(data);
-      } catch (err) {
-        setError((err as Error).message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProducts();
-  }, []);
-
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
-      {loading && <p>Loading products...</p>}
-      {error && <div className="error">Error: {error}</div>}
-
-      <ProductList products={products} />
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen bg-gray-100 p-6">
+        <h1 className="text-3xl font-bold mb-6 text-gray-900">
+          Product Catalog
+        </h1>
+        <ProductList />
+      </div>
+    </>
   );
 };
 
