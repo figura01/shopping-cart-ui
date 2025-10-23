46bcfd72771ff4ac01c5d052070303d763791608
laurentvuillaume <figura.graphik@gmail.com>
2025-10-24 01:17:20 +0200

    update main.tsx with contextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { ProductProvider } from "./context/ProductContext.tsx";
+import { CartProvider } from "./context/CartContext.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ProductProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductProvider>
+  </StrictMode>
+);
